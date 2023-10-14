853f1d3ff701a8d9e664d0f2398adac2d977b02e
shiva508 <dasarishiva1@gmail.com>
2023-10-14 11:34:07 +0530

    first commit

---
+++ src/main/java/com/comrade/SpringBootReactjsIntegrationMavenApplication.java
@@ -0,0 +1,13 @@
+package com.comrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactjsIntegrationMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactjsIntegrationMavenApplication.class, args);
+	}
+
+}
